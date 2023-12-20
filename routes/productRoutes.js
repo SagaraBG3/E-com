ded5d519876b40a78c7bbf7bccc3250d4aa2a8f5
SagaraBG3 <sagarabg3@gmail.com>
2023-12-20 23:14:13 +0530

    search and filter

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,13 @@ import {
   getProductController,
   getSingleProductController,
   productPhotoController,
+  productCountController,
+  productFiltersController,
+  productListController,
   updateProductController,
+  realtedProductController,
+  searchProductController,
+  productCategoryController,
 } from "../controllers/productController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import formidable from "express-formidable";
@@ -40,5 +46,27 @@ router.get("/product-photo/:pid", productPhotoController);
 
 //delete rproduct
 router.delete("/delete-product/:pid", deleteProductController);
+
+
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+
+//search product
+router.get("/search/:keyword", searchProductController);
+
+//similar product
+router.get("/related-product/:pid/:cid", realtedProductController);
+
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
+
+
 
 export default router;
