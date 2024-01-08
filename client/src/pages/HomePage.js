275c2ac39d3afde12dee2398ca49237658c7d232
SagaraBG3 <sagarabg3@gmail.com>
2024-01-09 00:01:03 +0530

    prev-final(changes)

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,6 +3,7 @@ import Layout from "./../components/Layout/Layout";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
+import "../styles/Homepage.css";
 
 
 import axios from "axios";
